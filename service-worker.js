8e2f064880482345886d6820a2ba00f6f61fa152
smackjax <smackjax@gmail.com>
2017-12-05 15:47:23 -0500

    Add /logout data reset to sw

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,8 @@
 self.importScripts('/app/localforage/localforage.min.js');
 // Minified app service worker. Must be replaced with new after each 'build'. 
 // TODO gotta figure out a better flow. This sucks.
-function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}var precacheConfig=[["/app/index.html","6358329d60b9ef3b2a97b6d06e9c9b85"],["/app/static/css/main.cbf756ba.css","aeaa4f1e71501b0b1ed352562bfc275c"],["/app/static/js/main.640d6495.js","780ec80784135c45ac027d4119310759"],["/app/static/media/breakfast-circle.fe6e18cc.svg","fe6e18cc7133a92d689f6b40fa1e030f"],["/app/static/media/chevron-down.1e47aae5.svg","1e47aae56fb52812f4d12de1d90acc52"],["/app/static/media/dessert-circle.5bebc8f4.svg","5bebc8f45d6b9390c76d8a0511839b20"],["/app/static/media/dinner-circle.43ae1fec.svg","43ae1fecdfc2cdc9d61fd098d8e39977"],["/app/static/media/drink-circle.efccca2b.svg","efccca2b90a73cf285dfd4223d2819d3"],["/app/static/media/fontawesome-webfont.674f50d2.eot","674f50d287a8c48dc19ba404d20fe713"],["/app/static/media/fontawesome-webfont.912ec66d.svg","912ec66d7572ff821749319396470bde"],["/app/static/media/fontawesome-webfont.af7ae505.woff2","af7ae505a9eed503f8b8e6982036873e"],["/app/static/media/fontawesome-webfont.b06871f2.ttf","b06871f281fee6b241d60582ae9369b9"],["/app/static/media/fontawesome-webfont.fee66e71.woff","fee66e712a8a08eef5805a46892932ad"],["/app/static/media/logo.1f0ee7bd.svg","1f0ee7bd37466f21677334ed0695e1be"],["/app/static/media/lunch-circle.6f0ad706.svg","6f0ad70604fce603cc83b4215bfa8398"],["/app/static/media/snack-circle.1ddd758e.svg","1ddd758e23d42fa2341935d4d0697f4c"],["/app/static/media/untyped-circle.5ef7b402.svg","5ef7b4020d21a52b76630edd066a2ea9"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var a=new URL(e);return"/"===a.pathname.slice(-1)&&(a.pathname+=t),a.toString()},cleanResponse=function(e){return e.redirected?("body"in e?Promise.resolve(e.body):e.blob()).then(function(t){return new Response(t,{headers:e.headers,status:e.status,statusText:e.statusText})}):Promise.resolve(e)},createCacheKey=function(e,t,a,n){var r=new URL(e);return n&&r.pathname.match(n)||(r.search+=(r.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(a)),r.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var a=new URL(t).pathname;return e.some(function(e){return a.match(e)})},stripIgnoredUrlParameters=function(e,t){var a=new URL(e);return a.hash="",a.search=a.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(e){return t.every(function(t){return!t.test(e[0])})}).map(function(e){return e.join("=")}).join("&"),a.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],a=e[1],n=new URL(t,self.location),r=createCacheKey(n,hashParamName,a,/\.\w{8}\./);return[n.toString(),r]}));self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(e){return setOfCachedUrls(e).then(function(t){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(a){if(!t.has(a)){var n=new Request(a,{credentials:"same-origin"});return fetch(n).then(function(t){if(!t.ok)throw new Error("Request for "+a+" returned a response with status "+t.status);return cleanResponse(t).then(function(t){return e.put(a,t)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var t=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(e){return e.keys().then(function(a){return Promise.all(a.map(function(a){if(!t.has(a.url))return e.delete(a)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(e){if("GET"===e.request.method){var t,a=stripIgnoredUrlParameters(e.request.url,ignoreUrlParametersMatching);(t=urlsToCacheKeys.has(a))||(a=addDirectoryIndex(a,"index.html"),t=urlsToCacheKeys.has(a));!t&&"navigate"===e.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],e.request.url)&&(a=new URL("/app/index.html",self.location).toString(),t=urlsToCacheKeys.has(a)),t&&e.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(a)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(t){return console.warn('Couldn\'t serve response for "%s" from cache: %O',e.request.url,t),fetch(e.request)}))}});
+function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}var precacheConfig=[["/app/index.html","075cb9020d0ba997f6925dbe3641bb9d"],["/app/static/css/main.89cbac0d.css","d0a107f4c8bfdcfb46914f68ebb8d8c3"],["/app/static/js/main.03d37b9d.js","5ed4f3eed385abe4a2a7a8886a220e4a"],["/app/static/media/breakfast-circle.fe6e18cc.svg","fe6e18cc7133a92d689f6b40fa1e030f"],["/app/static/media/chevron-down.1e47aae5.svg","1e47aae56fb52812f4d12de1d90acc52"],["/app/static/media/dessert-circle.5bebc8f4.svg","5bebc8f45d6b9390c76d8a0511839b20"],["/app/static/media/dinner-circle.43ae1fec.svg","43ae1fecdfc2cdc9d61fd098d8e39977"],["/app/static/media/drink-circle.efccca2b.svg","efccca2b90a73cf285dfd4223d2819d3"],["/app/static/media/fontawesome-webfont.674f50d2.eot","674f50d287a8c48dc19ba404d20fe713"],["/app/static/media/fontawesome-webfont.912ec66d.svg","912ec66d7572ff821749319396470bde"],["/app/static/media/fontawesome-webfont.af7ae505.woff2","af7ae505a9eed503f8b8e6982036873e"],["/app/static/media/fontawesome-webfont.b06871f2.ttf","b06871f281fee6b241d60582ae9369b9"],["/app/static/media/fontawesome-webfont.fee66e71.woff","fee66e712a8a08eef5805a46892932ad"],["/app/static/media/logo.1f0ee7bd.svg","1f0ee7bd37466f21677334ed0695e1be"],["/app/static/media/lunch-circle.6f0ad706.svg","6f0ad70604fce603cc83b4215bfa8398"],["/app/static/media/snack-circle.1ddd758e.svg","1ddd758e23d42fa2341935d4d0697f4c"],["/app/static/media/untyped-circle.5ef7b402.svg","5ef7b4020d21a52b76630edd066a2ea9"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var a=new URL(e);return"/"===a.pathname.slice(-1)&&(a.pathname+=t),a.toString()},cleanResponse=function(e){return e.redirected?("body"in e?Promise.resolve(e.body):e.blob()).then(function(t){return new Response(t,{headers:e.headers,status:e.status,statusText:e.statusText})}):Promise.resolve(e)},createCacheKey=function(e,t,a,n){var r=new URL(e);return n&&r.pathname.match(n)||(r.search+=(r.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(a)),r.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var a=new URL(t).pathname;return e.some(function(e){return a.match(e)})},stripIgnoredUrlParameters=function(e,t){var a=new URL(e);return a.hash="",a.search=a.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(e){return t.every(function(t){return!t.test(e[0])})}).map(function(e){return e.join("=")}).join("&"),a.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],a=e[1],n=new URL(t,self.location),r=createCacheKey(n,hashParamName,a,/\.\w{8}\./);return[n.toString(),r]}));self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(e){return setOfCachedUrls(e).then(function(t){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(a){if(!t.has(a)){var n=new Request(a,{credentials:"same-origin"});return fetch(n).then(function(t){if(!t.ok)throw new Error("Request for "+a+" returned a response with status "+t.status);return cleanResponse(t).then(function(t){return e.put(a,t)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var t=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(e){return e.keys().then(function(a){return Promise.all(a.map(function(a){if(!t.has(a.url))return e.delete(a)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(e){if("GET"===e.request.method){var t,a=stripIgnoredUrlParameters(e.request.url,ignoreUrlParametersMatching);(t=urlsToCacheKeys.has(a))||(a=addDirectoryIndex(a,"index.html"),t=urlsToCacheKeys.has(a));!t&&"navigate"===e.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],e.request.url)&&(a=new URL("/app/index.html",self.location).toString(),t=urlsToCacheKeys.has(a)),t&&e.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(a)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(t){return console.warn('Couldn\'t serve response for "%s" from cache: %O',e.request.url,t),fetch(e.request)}))}});
+
 
 /* NOTES: 
 'backlog' refers to the sync backlog,
@@ -349,23 +350,22 @@ var deleteFriends = function (token, idsToDelete){
 
 }
 
+var resetLocalData = function(){
+    return Promise.all([
+        setDbData(MAIN_APP_DATA_KEY, {}),
+        setDbData(RECIPES_SAVE_KEY, []),
+        setDbData(RECIPES_DELETE_KEY, []),
+        setDbData(FRIENDS_DELETE_KEY, []),
+        setDbData(TOKEN_KEY, "")
+    ])
+    .then(()=>{ console.log("Local data reset successful") })
+    .catch(err=>{ console.log("Local data reset failure: ", err) })
+}
+
 
 self.addEventListener("install", event=>{
-    // Initialize needed data to avoid any 'undefined' errors
-    event.waitUntil(
-        Promise.all([
-            setDbData(MAIN_APP_DATA_KEY, {}),
-            setDbData(RECIPES_SAVE_KEY, []).then(function(){
-                console.log("Initialized " + RECIPES_SAVE_KEY);
-                return true;
-            }),
-            setDbData(RECIPES_DELETE_KEY, []),
-            setDbData(FRIENDS_DELETE_KEY, []),
-            setDbData(TOKEN_KEY, "")
-        ])
-        .then(()=>{ console.log("Local data initialize successful") })
-        .catch(err=>{ console.log("Local data initailize failure: ", err) })
-    );
+    // Initialize needed data to minimize 'undefined' errors
+    event.waitUntil( resetLocalData() )
 });
 
 
@@ -376,7 +376,14 @@ self.addEventListener("fetch", function(event){
     
     // HANDLE SIGN-IN REQUEST
     if(/\/api\/login\//.test(pathname)){
-        if(pathname !== "/api/login/check-username"){//TODO don't like this hardcoding
+        
+        //TODO don't like this hardcoding
+        // Catch logout route
+        if(pathname === "/api/login/logout"){
+            event.waitUntil( resetLocalData() )
+        }   
+        // Catches both login routes that return a token
+        else if(pathname !== "/api/login/check-username"){
             event.respondWith(
                 fetch(event.request)
                 .then(function(response){
@@ -384,7 +391,6 @@ self.addEventListener("fetch", function(event){
                     return signInResponse.json()
                     .then(resJSON=>{
                         var JWT = resJSON.token;
-                        console.log("JWT to save: ", JWT);
                         return setDbData(TOKEN_KEY, JWT);
                     })
                     .catch(function(err){
